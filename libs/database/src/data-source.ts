d966b1a179b541ee9e4c22753a31d40b87b123f4
ThinhVinhPhat <thinhvinhp@gmail.com>
2026-03-30 23:08:02 +0700

    feat: enable SSL configuration for database connections

--- libs/database/src/data-source.ts
+++ libs/database/src/data-source.ts
@@ -12,9 +12,9 @@ export const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME || 'postgres',
   password: process.env.DB_PASSWORD || 'postgres',
   database: process.env.DB_NAME || 'zaloclone',
-  // ssl: {
-  //   rejectUnauthorized: false,
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+  },
   synchronize: false,
   logging: process.env.NODE_ENV !== 'production',
   entities: ['libs/database/src/entities/*.entity.ts'],
